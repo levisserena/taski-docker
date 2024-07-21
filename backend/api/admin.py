83bfc54d53df521fe0df976d63a6a1a5a9e87625
Lev Akchurin <levisserena@yandex.ru>
2024-07-21 22:10:56 +0400

    First review

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from .models import Task
 
 
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
 
-admin.site.register(Task, TaskAdmin)
+admin.site.site_title = 'Админ-зона Taski.'
+admin.site.site_header = 'Админ-зона Taski.'
